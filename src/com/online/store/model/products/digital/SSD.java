8e6d00bbfb0a00e764f9e99d41ee98f51c9321fa
parimd81 <prnyanmrady614@gmail.com>
2026-06-18 16:01:20 +0330

    Add digital product hierarchy

--- src/com/online/store/model/products/digital/SSD.java
+++ src/com/online/store/model/products/digital/SSD.java
@@ -1,13 +1,38 @@
 package com.online.store.model.products.digital;
 
+
 public class SSD extends DigitalProduct {
+
     private int capacityGB;
 
-    public SSD(String id, String name, double price, int stock, String brand, int capacityGB) {
+
+    public SSD(String id,
+               String name,
+               double price,
+               int stock,
+               String brand,
+               int capacityGB) {
+
         super(id, name, price, stock, brand);
+
         this.capacityGB = capacityGB;
     }
 
-    public int getCapacityGB() { return capacityGB; }
-    public void setCapacityGB(int capacityGB) { this.capacityGB = capacityGB; }
+
+    public int getCapacityGB() {
+        return capacityGB;
+    }
+
+
+    public void setCapacityGB(int capacityGB) {
+        this.capacityGB = capacityGB;
+    }
+
+
+    @Override
+    public String toString() {
+        return "SSD{" +
+                "capacityGB=" + capacityGB +
+                '}';
+    }
 }
