00a2da7dc2f48436ed892cefcf3abdf0af60bcea
parimd81 <prnyanmrady614@gmail.com>
2026-06-18 15:33:37 +0330

    Complete User abstract class

--- src/com/online/store/model/users/User.java
+++ src/com/online/store/model/users/User.java
@@ -7,14 +7,63 @@ public abstract class User {
     private String phoneNumber;
     private String password;
 
-    public User(String username,
-                String email,
-                String phoneNumber,
-                String password) {
 
+    // Constructor
+    public User(String username, String email, String phoneNumber, String password) {
         this.username = username;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
+    }
+
+
+    // Getter و Setter مربوط به username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    // Getter و Setter مربوط به email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    // Getter و Setter مربوط به phoneNumber
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    // Getter و Setter مربوط به password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    // نمایش اطلاعات کاربر
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 }
