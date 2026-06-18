9cfadf7893d81f5fcba53c7184b4bb6900fef85f
parimd81 <prnyanmrady614@gmail.com>
2026-06-18 16:39:59 +0330

    Add SessionManager singleton

---
+++ src/com/online/store/utils/SessionManager.java
@@ -0,0 +1,64 @@
+package com.online.store.utils;
+
+
+import com.online.store.model.users.User;
+
+
+public class SessionManager {
+
+
+    private static SessionManager instance;
+
+
+    private User currentUser;
+
+
+
+    private SessionManager() {
+
+    }
+
+
+
+    public static SessionManager getInstance() {
+
+
+        if(instance == null) {
+
+            instance = new SessionManager();
+
+        }
+
+
+        return instance;
+    }
+
+
+
+    public User getCurrentUser() {
+
+        return currentUser;
+    }
+
+
+
+    public void setCurrentUser(User user) {
+
+        this.currentUser = user;
+    }
+
+
+
+    public void logout() {
+
+        this.currentUser = null;
+    }
+
+
+
+    public boolean isLoggedIn() {
+
+        return currentUser != null;
+    }
+
+}
