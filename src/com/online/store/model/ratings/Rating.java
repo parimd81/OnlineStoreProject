66f8f5acf77e250de9ae54f5f6475d632fa07721
parimd81 <prnyanmrady614@gmail.com>
2026-06-18 16:34:25 +0330

    Add Rating model

---
+++ src/com/online/store/model/ratings/Rating.java
@@ -0,0 +1,112 @@
+package com.online.store.model.ratings;
+
+
+import com.online.store.model.products.Product;
+import com.online.store.model.users.Buyer;
+
+
+public class Rating {
+
+
+    private int id;
+
+    private Buyer buyer;
+
+    private Product product;
+
+    private int score;
+
+
+
+    public Rating(int id,
+                  Buyer buyer,
+                  Product product,
+                  int score) {
+
+
+        this.id = id;
+        this.buyer = buyer;
+        this.product = product;
+
+
+        if(score < 1 || score > 5) {
+            throw new IllegalArgumentException(
+                    "Rating score must be between 1 and 5"
+            );
+        }
+
+
+        this.score = score;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+
+
+    public Product getProduct() {
+        return product;
+    }
+
+
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+
+    public int getScore() {
+        return score;
+    }
+
+
+
+    public void setScore(int score) {
+
+
+        if(score < 1 || score > 5) {
+
+            throw new IllegalArgumentException(
+                    "Rating score must be between 1 and 5"
+            );
+        }
+
+
+        this.score = score;
+    }
+
+
+
+    @Override
+    public String toString() {
+
+        return "Rating{" +
+                "id=" + id +
+                ", buyer=" + buyer.getUsername() +
+                ", product=" + product.getName() +
+                ", score=" + score +
+                '}';
+    }
+}
