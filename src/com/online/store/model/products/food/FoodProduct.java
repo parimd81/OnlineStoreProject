19dfa416a5c4965ecb55e364e49f37e0c3462a0f
parimd81 <prnyanmrady614@gmail.com>
2026-06-18 16:12:41 +0330

    Add food product class

--- src/com/online/store/model/products/food/FoodProduct.java
+++ src/com/online/store/model/products/food/FoodProduct.java
@@ -1,15 +1,54 @@
 package com.online.store.model.products.food;
 
+import com.online.store.model.products.Product;
+
 import java.time.LocalDate;
 
 public class FoodProduct extends Product {
-    private LocalDate expiryDate;
 
-    public FoodProduct(String id, String name, double price, int stock, LocalDate expiryDate) {
+    private LocalDate productionDate;
+    private LocalDate expirationDate;
+
+
+    public FoodProduct(String id,
+                       String name,
+                       double price,
+                       int stock,
+                       LocalDate productionDate,
+                       LocalDate expirationDate) {
+
         super(id, name, price, stock);
-        this.expiryDate = expiryDate;
+
+        this.productionDate = productionDate;
+        this.expirationDate = expirationDate;
     }
 
-    public LocalDate getExpiryDate() { return expiryDate; }
-    public void setExpiryDate(LocalDate expiryDate) { this.expiryDate = expiryDate; }
+
+    public LocalDate getProductionDate() {
+        return productionDate;
+    }
+
+
+    public void setProductionDate(LocalDate productionDate) {
+        this.productionDate = productionDate;
+    }
+
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "FoodProduct{" +
+                "productionDate=" + productionDate +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
 }
